f5602f96c454e6a78a8b0abcbd35e4c51fd17f46
DAN FOREJTEK <dan.forejtek@mercedes-benz.com>
2023-12-01 20:56:13 +0100

    fix route

--- app/api/latest/route.ts
+++ app/api/latest/route.ts
@@ -6,6 +6,7 @@ export async function GET(request: Request) {
   const res = await fetch(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=${10}`, {
     next: { revalidate: 60 * 10 }, // in seconds
     // cache: "no-cache", // Don't cache response
+    // @ts-ignore
     headers: {
       "Content-Type": "application/json",
       "X-CMC_PRO_API_KEY": process.env.COINMARKETCAP_API_KEY,
