6d2d6b1b780a4ef6ac23cecee37ce11e18fa9e79
DAN FOREJTEK <d.forejtek@gmail.com>
2023-12-08 17:44:05 +0100

    t

--- components/CryptoStats.tsx
+++ components/CryptoStats.tsx
@@ -3,29 +3,34 @@ import CryptoTable from "./CryptoTable"
 const getData = async () => {
   try {
     // const response = await fetch(`https://api.coingarage.io/market/market-data`, { cache: "no-cache" })
-    const response = await fetch(`https://api.coingarage.io/market/market-data`, { next: { revalidate: 60 * 9 } })
+    const response = await fetch(`https://api.coingarage.io/market/market-data`, {
+      headers: { "Content-Type": "application/json" },
+      next: { revalidate: 60 * 9 },
+    })
+    console.log(response.clone().text())
     const data = await response.json()
-    console.log("coingaragedata", data)
-    const market = data?.crypto_market
-    const trending = Object.keys(data?.trending)
-      .filter((item) => item.split("_")[1] === "USDT")
-      .map((item) => {
-        return { name: item.split("_")[0], ...market[item] }
-      })
-    const topGainers = Object.keys(data?.top_gainer)
-      .filter((item) => item.split("_")[1] === "USDT")
-      .map((item) => {
-        return { name: item.split("_")[0], ...market[item] }
-      })
-    const recentlyAdded = Object.keys(data?.recently_added)
-      .filter((item) => item.split("_")[1] === "USDT")
-      .map((item) => {
-        return { name: item.split("_")[0], ...market[item] }
-      })
-    return { trending, topGainers, recentlyAdded }
+    if (data) {
+      const market = data?.crypto_market
+      const trending = Object.keys(data?.trending)
+        .filter((item) => item.split("_")[1] === "USDT")
+        .map((item) => {
+          return { name: item.split("_")[0], ...market[item] }
+        })
+      const topGainers = Object.keys(data?.top_gainer)
+        .filter((item) => item.split("_")[1] === "USDT")
+        .map((item) => {
+          return { name: item.split("_")[0], ...market[item] }
+        })
+      const recentlyAdded = Object.keys(data?.recently_added)
+        .filter((item) => item.split("_")[1] === "USDT")
+        .map((item) => {
+          return { name: item.split("_")[0], ...market[item] }
+        })
+      return { trending, topGainers, recentlyAdded }
+    }
   } catch (error) {
     console.log("WHY THIS SHIT FAILS ON FUCNKING VERCEL")
-    console.log(error)
+    console.log(JSON.stringify(error))
     return { trending: [], topGainers: [], recentlyAdded: [] }
   }
 }
