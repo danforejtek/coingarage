19af79d46cc2a1c747ed752a5324f8b75069a5e2
DAN FOREJTEK <d.forejtek@gmail.com>
2023-12-09 16:32:16 +0100

    test

--- app/api/crypto-market/route.ts
+++ app/api/crypto-market/route.ts
@@ -1,28 +1,22 @@
 export async function GET(request: Request) {
   try {
     // const response = await fetch(`https://api.coingarage.io/market/market-data`, { cache: "no-cache" })
-    const response = await fetch(`https://api.coingarage.io/market/market-data`, {
+    const response = await fetch(`https://api.coingarage.io/market/market-data/usdt`, {
       headers: { "Content-Type": "application/json" },
       cache: "no-cache",
     })
     const data = await response.json()
     if (typeof data === "object") {
       const market = data?.crypto_market
-      const trending = Object.keys(data?.trending)
-        .filter((item) => item.split("_")[1] === "USDT")
-        .map((item) => {
-          return { name: item.split("_")[0], ...market[item] }
-        })
-      const topGainers = Object.keys(data?.top_gainer)
-        .filter((item) => item.split("_")[1] === "USDT")
-        .map((item) => {
-          return { name: item.split("_")[0], ...market[item] }
-        })
-      const recentlyAdded = Object.keys(data?.recently_added)
-        .filter((item) => item.split("_")[1] === "USDT")
-        .map((item) => {
-          return { name: item.split("_")[0], ...market[item] }
-        })
+      const trending = Object.keys(data?.trending).map((item) => {
+        return { name: item.split("_")[0], ...market[item] }
+      })
+      const topGainers = Object.keys(data?.top_gainer).map((item) => {
+        return { name: item.split("_")[0], ...market[item] }
+      })
+      const recentlyAdded = Object.keys(data?.recently_added).map((item) => {
+        return { name: item.split("_")[0], ...market[item] }
+      })
       return Response.json({ trending, topGainers, recentlyAdded })
     }
     return Response.json({ trending: [], topGainers: [], recentlyAdded: [] })
--- components/CryptoStats.tsx
+++ components/CryptoStats.tsx
@@ -4,53 +4,54 @@ import CryptoTable from "./CryptoTable"
 import { useEffect, useState } from "react"
 import { Skeleton } from "@/components/ui/skeleton"
 
-const parseData = (data) => {
-  try {
-    if (typeof data === "object") {
-      const market = data?.crypto_market
-      const trending = Object.keys(data?.trending).map((item) => {
-        return { name: item.split("_")[0], ...market[item] }
-      })
-      const topGainers = Object.keys(data?.top_gainer).map((item) => {
-        return { name: item.split("_")[0], ...market[item] }
-      })
-      const recentlyAdded = Object.keys(data?.recently_added).map((item) => {
-        return { name: item.split("_")[0], ...market[item] }
-      })
-      return { trending, topGainers, recentlyAdded }
-    }
-    return { trending: [], topGainers: [], recentlyAdded: [] }
-  } catch (error) {
-    return { trending: [], topGainers: [], recentlyAdded: [] }
-  }
-}
+// const parseData = (data) => {
+//   try {
+//     if (typeof data === "object") {
+//       const market = data?.crypto_market
+//       const trending = Object.keys(data?.trending).map((item) => {
+//         return { name: item.split("_")[0], ...market[item] }
+//       })
+//       const topGainers = Object.keys(data?.top_gainer).map((item) => {
+//         return { name: item.split("_")[0], ...market[item] }
+//       })
+//       const recentlyAdded = Object.keys(data?.recently_added).map((item) => {
+//         return { name: item.split("_")[0], ...market[item] }
+//       })
+//       return { trending, topGainers, recentlyAdded }
+//     }
+//     return { trending: [], topGainers: [], recentlyAdded: [] }
+//   } catch (error) {
+//     return { trending: [], topGainers: [], recentlyAdded: [] }
+//   }
+// }
 
 const fetcher = (...args) =>
-  fetch(...args, { headers: { "Content-Type": "application/json" } }).then((res) => res.json())
+  fetch(...args, { headers: { "Content-Type": "application/json" }, mode: "no-cors" }).then((res) => res.json())
 
 export default function CryptoStats() {
-  const [parsedData, setParsedData] = useState({ trending: [], topGainers: [], recentlyAdded: [] })
-  const { data, error, isLoading } = useSWR("https://api.coingarage.io/market/market-data/usdt", fetcher)
+  // const [parsedData, setParsedData] = useState({ trending: [], topGainers: [], recentlyAdded: [] })
+  // const { data, error, isLoading } = useSWR("https://api.coingarage.io/market/market-data/usdt", fetcher)
+  const { data, error, isLoading } = useSWR("/api/crypto-market", fetcher)
 
-  useEffect(() => {
-    if (data) {
-      setParsedData(parseData(data))
-    }
-  }, [data, error, isLoading])
+  // useEffect(() => {
+  //   if (data) {
+  //     setParsedData(parseData(data))
+  //   }
+  // }, [data, error, isLoading])
 
   return (
     <div className="grid grid-cols-1 gap-12 md:grid-cols-2 xl:grid-cols-3 xl:gap-16">
-      {false ? (
+      {!isLoading ? (
         <>
-          <CryptoTable heading="Trending" data={parsedData?.trending} />
-          <CryptoTable heading="Top Gainers" data={parsedData?.topGainers} />
-          <CryptoTable heading="Recently Added" data={parsedData?.recentlyAdded} />
+          <CryptoTable heading="Trending" data={data?.trending} />
+          <CryptoTable heading="Top Gainers" data={data?.topGainers} />
+          <CryptoTable heading="Recently Added" data={data?.recentlyAdded} />
         </>
       ) : (
         <>
-          <Skeleton className="h-[400px] w-full rounded-2xl" />
-          <Skeleton className="h-[400px] w-full rounded-2xl" />
-          <Skeleton className="h-[400px] w-full rounded-2xl" />
+          <Skeleton className="h-[532px] w-full rounded-2xl" />
+          <Skeleton className="h-[532px] w-full rounded-2xl" />
+          <Skeleton className="h-[532px] w-full rounded-2xl" />
         </>
       )}
     </div>
