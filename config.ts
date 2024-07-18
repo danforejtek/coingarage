d9c67c629bfd8597c26958cbc7beece67ff42200
DAN FOREJTEK <d.forejtek@gmail.com>
2024-07-18 11:41:18 +0200

    locales

--- config.ts
+++ config.ts
@@ -1,8 +1,11 @@
-export const locales = ["en", "cs", "de", "es"]
+export const locales = ["en", "cs", "de", "es", "it", "gr", "tr"]
 export const defaultLocale = "en"
 export const localesLabel = {
   en: "EN",
   cs: "CZ",
   de: "DE",
   es: "ES",
+  it: "IT",
+  gr: "GR",
+  tr: "TR",
 }
