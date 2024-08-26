6dc791bf20ef766f1fd22daa14f46108f1f3fffa
DAN FOREJTEK <d.forejtek@gmail.com>
2024-08-26 11:53:32 +0200

    disable telegram api

--- app/api/news/route.ts
+++ app/api/news/route.ts
@@ -1,41 +1,41 @@
-import { Logger, TelegramClient } from "telegram"
-import { LogLevel } from "telegram/extensions/Logger"
-import { StringSession } from "telegram/sessions"
+// import { Logger, TelegramClient } from "telegram"
+// import { LogLevel } from "telegram/extensions/Logger"
+// import { StringSession } from "telegram/sessions"
 
-const TELEGRAM_API_ID = process.env.TELEGRAM_API_ID || 0
-const TELEGRAM_API_HASH = process.env.TELEGRAM_API_HASH || ""
-const TELEGRAM_STRING_SESSION = process.env.TELEGRAM_STRING_SESSION
+// const TELEGRAM_API_ID = process.env.TELEGRAM_API_ID || 0
+// const TELEGRAM_API_HASH = process.env.TELEGRAM_API_HASH || ""
+// const TELEGRAM_STRING_SESSION = process.env.TELEGRAM_STRING_SESSION
 
-const session = new StringSession(TELEGRAM_STRING_SESSION)
-const client = new TelegramClient(session, Number(TELEGRAM_API_ID), TELEGRAM_API_HASH, {
-  baseLogger: new Logger(LogLevel.ERROR),
-})
+// const session = new StringSession(TELEGRAM_STRING_SESSION)
+// const client = new TelegramClient(session, Number(TELEGRAM_API_ID), TELEGRAM_API_HASH, {
+//   baseLogger: new Logger(LogLevel.ERROR),
+// })
 
+// // export const revalidate = 60 * 10 // 10 minutes
 // export const revalidate = 60 * 10 // 10 minutes
-export const revalidate = 60 * 10 // 10 minutes
 
-export async function GET() {
-  try {
-    await client.connect().catch((error) => {
-      console.log(error)
-    })
+// export async function GET() {
+//   try {
+//     await client.connect().catch((error) => {
+//       console.log(error)
+//     })
 
-    if (client?.session) {
-      const messages = []
-      for await (const message of client.iterMessages("www_Bitcoin_com", {
-        limit: 3,
-      })) {
-        const rawText = message.text
-        const text = rawText.split(" […]\n")[0]
-        const url = rawText.match(/(https?:\/\/[^\s]+)/g)?.[0]
-        messages.push({ id: message.id, timestamp: new Date(message.date * 1000)?.toISOString(), text, url })
-      }
-      return Response.json(messages)
-    } else {
-      throw new Error("Telegram client not connected")
-    }
-  } catch (error) {
-    console.log(error)
-    return Response.json([])
-  }
-}
+//     if (client?.session) {
+//       const messages = []
+//       for await (const message of client.iterMessages("www_Bitcoin_com", {
+//         limit: 3,
+//       })) {
+//         const rawText = message.text
+//         const text = rawText.split(" […]\n")[0]
+//         const url = rawText.match(/(https?:\/\/[^\s]+)/g)?.[0]
+//         messages.push({ id: message.id, timestamp: new Date(message.date * 1000)?.toISOString(), text, url })
+//       }
+//       return Response.json(messages)
+//     } else {
+//       throw new Error("Telegram client not connected")
+//     }
+//   } catch (error) {
+//     console.log(error)
+//     return Response.json([])
+//   }
+// }
