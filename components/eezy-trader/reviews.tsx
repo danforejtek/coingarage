579363ac9c6666c8468c2aabfe5314c1f5fc31af
Jan Gross <jan.gross.cze@gmail.com>
2024-09-02 19:46:04 +0200

    test

--- components/eezy-trader/reviews.tsx
+++ components/eezy-trader/reviews.tsx
@@ -26,15 +26,15 @@ const Reviews = () => {
   return (
     <div className="mt-28">
       <div className="mb-12 text-center">
-        <h3 className="font-heading text-3xl font-bold text-primary">{t("subheader")}</h3>
-        <h3 className="font-heading text-4xl font-bold">{t("header")}</h3>
+        <h3 className="font-heading text-3xl text-primary">{t("subheader")}</h3>
+        <h3 className="font-heading text-4xl">{t("header")}</h3>
       </div>
 
       <div className="mt-0">
         <Carousel opts={{ align: "start", loop: true }} className="w-full">
-          <CarouselContent className="mb-4 h-full pb-8">
+          <CarouselContent className="h-full pb-8">
             {referencesArray.map((src, index) => (
-              <CarouselItem key={index} className="basis-1/2 md:basis-1/3 xl:basis-1/4 2xl:basis-1/5">
+              <CarouselItem key={index} className="-pb-8 -mb-3 sm:basis-1/2 md:basis-1/3 xl:basis-1/4 2xl:basis-1/5">
                 <Card className="mx-auto flex h-1/2 w-full flex-col p-6 md:min-h-96">
                   <div className="flex justify-between">
                     <Image src="/images/eezy-trader/icons/quote.svg" width={40} height={40} alt="" />
@@ -44,12 +44,12 @@ const Reviews = () => {
                     </div>
                   </div>
                   <div className="mt-4">
-                    <p className="mb-4 min-h-14 text-xl font-bold leading-tight">{src.name}</p>
-                    <p className="italic">{src.text}</p>
+                    <p className="mb-4 text-[24px]"> {src.name} </p>
+                    <p className="italic"> {src.text} </p>
                   </div>
                 </Card>
                 {isTablet && (
-                  <Card className="mx-auto flex h-1/2 w-full flex-col p-6 md:min-h-96">
+                  <Card className="mx-auto mt-4 flex h-1/2 w-full flex-col p-6 md:min-h-96">
                     <div className="flex justify-between">
                       <Image src="/images/eezy-trader/icons/quote.svg" width={40} height={40} alt="" />
                       <div className="flex w-14 items-center justify-center rounded-3xl bg-primary text-white">
@@ -58,8 +58,8 @@ const Reviews = () => {
                       </div>
                     </div>
                     <div className="mt-4">
-                      <p className="mb-4 min-h-14 text-xl font-bold leading-tight">{src.name}</p>
-                      <p className="italic">{src.text}</p>
+                      <p className="mb-4 text-[24px]"> {src.name} </p>
+                      <p className="italic"> {src.text} </p>
                     </div>
                   </Card>
                 )}
