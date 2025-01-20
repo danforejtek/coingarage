d8510cee0b3556d8de3493a435b5b8a309e9b79b
Florcz <flor@flor.cz>
2025-01-20 20:41:59 +0100

    calculate with frequency, fetchin optimalization, correct display of valuation

--- app/(main)/[locale]/(coingarage)/earn/btc-saving-plan/btc-loss-graph.tsx
+++ app/(main)/[locale]/(coingarage)/earn/btc-saving-plan/btc-loss-graph.tsx
@@ -3,8 +3,10 @@ import * as React from "react"
 import { LineChart } from "@mui/x-charts/LineChart"
 import { LineSeriesType } from "@mui/x-charts"
 import { useTranslations } from "next-intl"
+import { useState } from "react"
 
 const yearFormatter = (date: Date) => date.toLocaleDateString()
+const monthFormatter = (date: Date) => date.toLocaleDateString("default", { month: "short" }) + " " + date.getFullYear()
 const thousandsFormatter = (value: string) => {
   const valueNum = parseInt(value)
   if (valueNum > 1000000) {
@@ -16,9 +18,23 @@ const thousandsFormatter = (value: string) => {
   }
 }
 
+// convert weekly data to monthly data
+const convertToMonthly = (data: any) => {
+  let savedMonth = 0
+  let monthlyData = []
+  for (let i = 0; i < data.length; i++) {
+    let actualMonth: number = new Date(data[i].time).getMonth()
+    if (actualMonth !== savedMonth) {
+      monthlyData.push(data[i])
+      savedMonth = actualMonth
+    }
+  }
+  return monthlyData
+}
+
 type Props = {
   amount: string
-  frequency: string
+  frequency: number
   dateOpening: string
   dateClosing: string
 }
@@ -28,24 +44,38 @@ function formatInUSD(amount: number | null) {
   return amount.toLocaleString("en-US", { style: "currency", currency: "USD" })
 }
 
+// api returns 250 weeks of data at once
+// calculate the date 249 weeks ago from today (to have some overlap with the data)
+const minDate = new Date()
+minDate.setDate(minDate.getDate() - 249 * 7)
+
+// how this component works:
+// fetch intial btc data from the api -  from actual date to 250 weeks ago (api limit)
+// if user sets dateOpening earlier than minDate, fetch the rest of the data
+// there will be no more fetching, at this moment all BTC data is stored in mainData
+// if a user sets different time span, slice the mainData
+// calculate the the savings time lines and display them on the graph
+
 export default function BtcLossGraph({ amount, frequency, dateOpening, dateClosing }: Props) {
   const t = useTranslations("eezy-trader.BtcSaving")
-  const [series, setSeries] = React.useState<LineSeriesType[]>([])
-  const [dates, setDates] = React.useState<Date[]>([])
-  const [nbSeries, setNbSeries] = React.useState(1)
+  const [series, setSeries] = useState<LineSeriesType[]>([])
+  const [dates, setDates] = useState<Date[]>([])
+  const [nbSeries, setNbSeries] = useState(1)
   const amountNum: number = parseInt(amount)
-  const dateClosingDate = new Date(dateClosing).getTime()
   const dateOpeningDate = new Date(dateOpening).getTime()
-  const [btcPosY, setBtcPosY] = React.useState<number>(0)
-  const [conPosY, setConPosY] = React.useState<number>(0)
-  const [deposit, setDeposit] = React.useState<number>(0)
-  const [btcReturn, setBtcReturn] = React.useState<number>(0)
-  const [conReturn, setConReturn] = React.useState<number>(0)
-
-  const [data, setData] = React.useState<any>([])
+  const dateClosingDate = new Date(dateClosing).getTime() + 14 * 24 * 60 * 60 * 1000
+  const [btcPosY, setBtcPosY] = useState<number>(0)
+  const [conPosY, setConPosY] = useState<number>(0)
+  const [deposit, setDeposit] = useState<number>(0)
+  const [btcReturn, setBtcReturn] = useState<number>(0)
+  const [conReturn, setConReturn] = useState<number>(0)
+  const [refreshChart, setRefreshChart] = useState<boolean>(false)
+  const [noMoreFetching, setNoMoreFetching] = useState<boolean>(false)
+  // store the fetched graph data
+  const [mainData, setMainData] = useState<any>([])
 
   // day interval 1440 , week interval 10080, month interval 43200
-  const url = `https://api.coingarage.io/market/charts?base=BTC&quote=USDT&ts=${new Date().getTime()}&interval=43200`
+  const url = `https://api.coingarage.io/market/charts?base=BTC&quote=USDT&ts=${new Date().getTime()}&interval=10080`
 
   React.useEffect(() => {
     // fetch data once when the page loads and store them for future use
@@ -53,7 +83,7 @@ export default function BtcLossGraph({ amount, frequency, dateOpening, dateClosi
       try {
         const response = await fetch(url)
         let data = await response.json()
-        setData(data.reverse())
+        setMainData(data.reverse())
       } catch (error) {
         console.error("Error fetching chart data:", error)
       }
@@ -63,42 +93,96 @@ export default function BtcLossGraph({ amount, frequency, dateOpening, dateClosi
   }, [])
 
   React.useEffect(() => {
+    // if user sets dateOpening earlier than minDate, fetch more data
+    // but do it only once, noMoreFetching prevents another data fetch
+
+    if (new Date(dateOpening).getTime() < minDate.getTime() && !noMoreFetching) {
+      const fetchData = async () => {
+        try {
+          const response = await fetch(
+            `https://api.coingarage.io/market/charts?base=BTC&quote=USDT&ts=${minDate.getTime()}&interval=10080`
+          )
+          let data = await response.json()
+
+          data = data.reverse()
+          // find the index where the new data should be merged
+          const mergeIndex = data.findIndex((entry: any) => entry.time == mainData[0].time)
+          // merge the new data with the old data
+          setMainData([...data.slice(0, mergeIndex), ...mainData])
+          setNoMoreFetching(true)
+        } catch (error) {
+          console.error("Error fetching chart data:", error)
+        }
+      }
+
+      fetchData()
+    }
+  }, [dateOpening])
+
+  React.useEffect(() => {
     // if amount is not a number, dont try to calculate the chart
-    if (isNaN(amountNum) || data.length === 0) {
+    if (isNaN(amountNum) || mainData.length === 0) {
       setSeries([])
       setNbSeries(1)
       return
     }
 
-    const savingAccountInterest = 0.03
+    const savingAccountInterest = 1 + 0.03 / 12
+    let freq = frequency
 
-    let indexOpen = data.findIndex((entry: any) => entry.time >= dateOpeningDate)
-    let indexClose = data.findIndex((entry: any) => entry.time >= dateClosingDate)
-    const dataFiltered = [...data.slice(indexOpen, indexClose)]
-
-    const dataLength = dataFiltered.length
-    let conservativeArray: number[] = []
-    conservativeArray[0] = amountNum
-    let btcArray: number[] = []
-    btcArray[0] = amountNum / dataFiltered[0].close
-
-    let btcUsdArary: number[] = []
-    // count conservative sum during the time
-    for (let i = 1; i < dataLength; i++) {
-      let currentAmount: number = Math.round((conservativeArray[i - 1] + amountNum) * (1 + savingAccountInterest / 12))
-      conservativeArray.push(currentAmount)
+    let indexOpen = mainData.findIndex((entry: any) => entry.time >= dateOpeningDate)
+    let indexClose = mainData.findIndex((entry: any) => entry.time >= dateClosingDate)
+    let slicedData = [...mainData.slice(indexOpen, indexClose)]
+    if (frequency === 4) {
+      slicedData = convertToMonthly(slicedData)
+      freq = 1
     }
+
+    const dataLength = slicedData.length
+    let conservativeArray: number[] = []
+    let btcArray: number[] = []
+    let datesArray: Date[] = []
+    let btcUsdArary: number[] = []
+
+    // if monthly frequency, we add interest once a month
+    // if weekly or double weekly frequency, we add interest every 4 weeks
+    const everyFourWeeks = frequency === 4 ? 1 : 4
+
+    // count conservative sum during the time
+    for (let i = 0; i < dataLength; i += freq) {
+      if (i === 0) {
+        conservativeArray[0] = amountNum * savingAccountInterest
+      } else {
+        let currentAmount: number = 0
+        if (i % everyFourWeeks === 0) {
+          // once a month we add interest
+          currentAmount = (conservativeArray[i / freq - 1] + amountNum) * savingAccountInterest
+        } else {
+          currentAmount = conservativeArray[i / freq - 1] + amountNum
+        }
+        conservativeArray.push(currentAmount)
+      }
+    }
+
     // count btc sum during the time
-    for (let i = 1; i < dataLength; i++) {
-      let currentBtcAmount: number = btcArray[i - 1] + amountNum / dataFiltered[i].close
-      btcArray.push(currentBtcAmount)
+    for (let i = 0; i < dataLength; i += freq) {
+      // if frequency is 4 and i=8 then we are at the second iteration (8/4 = 2)
+      if (i === 0) {
+        btcArray[0] = amountNum / slicedData[0].close
+        datesArray[0] = new Date(slicedData[0].time)
+      } else {
+        let currentBtcAmount: number = btcArray[i / freq - 1] + amountNum / slicedData[i].close
+        btcArray.push(currentBtcAmount)
+        // also dates has to be pushed in the right frequency
+        datesArray.push(new Date(slicedData[i].time))
+      }
     }
     // convert btc sum to usd
-    for (let i = 0; i < dataLength; i++) {
-      btcUsdArary[i] = Math.round(btcArray[i] * dataFiltered[i].close)
+    for (let i = 0; i < btcArray.length; i++) {
+      btcUsdArary[i] = Math.round(btcArray[i] * slicedData[i * freq].close)
     }
 
-    setDates(dataFiltered.map((entry: any) => new Date(entry.time)))
+    setDates(datesArray)
     setSeries([
       {
         id: "1",
@@ -123,20 +207,25 @@ export default function BtcLossGraph({ amount, frequency, dateOpening, dateClosi
     ])
     setTimeout(() => {
       setNbSeries(2)
+      setTimeout(() => {
+        setRefreshChart((prev) => !prev)
+      }, 222)
     }, 2000)
-    setDeposit(parseInt(amount) * dataFiltered.length)
+
+    // conservative array and btc array has the right length - number of records in the chart
+    setDeposit(parseInt(amount) * conservativeArray.length)
     setBtcReturn(btcUsdArary[btcUsdArary.length - 1])
     setConReturn(conservativeArray[conservativeArray.length - 1])
-  }, [amount, frequency, dateOpening, dateClosing])
+  }, [amount, frequency, dateOpening, dateClosing, mainData.length])
 
-  React.useEffect(() => {
+  // calculate the position of the labels
+  React.useLayoutEffect(() => {
     const conservative = document.querySelector(".MuiLineElement-series-1")
     const btc = document.querySelector(".MuiLineElement-series-2")
 
-    setBtcPosY(btc?.getBoundingClientRect().height ?? 0)
-    setConPosY(conservative?.getBoundingClientRect().height ?? 0)
-    
-  }, [amount, frequency, dateOpening, dateClosing])
+    setBtcPosY(btc?.getBoundingClientRect().height ?? -333)
+    setConPosY(conservative?.getBoundingClientRect().height ?? -333)
+  }, [amount, frequency, dateOpening, dateClosing, refreshChart])
 
   return (
     <div className="relative w-full">
@@ -170,11 +259,11 @@ export default function BtcLossGraph({ amount, frequency, dateOpening, dateClosi
           )}
         </div>
         <div>
-          <span className="absolute right-5" style={{ bottom: conPosY + 30 + "px" }}>
-            {Math.round(conReturn / deposit * 100)} %
+          <span className="absolute right-5 text-[#00ABFE]" style={{ bottom: conPosY + 50 + "px" }}>
+            {isNaN(conReturn) ? "" : Math.round((conReturn / deposit - 1) * 1000) / 10 + " %"}
           </span>
-          <span className="absolute right-5" style={{ bottom: btcPosY + 30 + "px" }}>
-            {Math.round(btcReturn / deposit * 100)} %
+          <span className="absolute right-5 text-[#3FCC88]" style={{ bottom: btcPosY + 50 + "px" }}>
+            {isNaN(btcReturn) ? "" : Math.round((btcReturn / deposit - 1) * 1000) / 10 + " %"}
           </span>
           <LineChart
             yAxis={[{ tickLabelStyle: { fill: "white", fontWeight: "lighter" }, valueFormatter: thousandsFormatter }]}
@@ -183,7 +272,7 @@ export default function BtcLossGraph({ amount, frequency, dateOpening, dateClosi
                 tickLabelStyle: { fill: "white", fontWeight: "lighter" },
                 data: dates,
                 scaleType: "time",
-                valueFormatter: yearFormatter,
+                valueFormatter: frequency === 4 ? monthFormatter : yearFormatter,
               },
             ]}
             series={[...series.slice(0, nbSeries)]}
--- app/(main)/[locale]/(coingarage)/earn/btc-saving-plan/btc-loss-section.tsx
+++ app/(main)/[locale]/(coingarage)/earn/btc-saving-plan/btc-loss-section.tsx
@@ -14,12 +14,20 @@ import {
 import { useTranslations } from "next-intl"
 import { useState } from "react"
 
+// api returns 250 weeks of data at once
+// I calculate with 244 so the defaultDate date is bigger then minDate used in graph component
+// there has to be big reserve, becasue defaultDate is stripped of days and begins at the start of the month
+const defaultDate = new Date()
+defaultDate.setDate(defaultDate.getDate() - 244 * 7)
+
 export default function BtcLossSection() {
   const t = useTranslations("eezy-trader.BtcSaving")
   const [amount, setAmount] = useState("")
-  const [frequency, setFrequency] = useState("monthly")
-  const [dateOpening, setDateOpening] = useState("2020-01-01")
-  const [dateClosing, setDateClosing] = useState(new Date().toISOString().split("T")[0])
+  const [frequency, setFrequency] = useState("4")
+
+  const [dateOpening, setDateOpening] = useState(defaultDate.toISOString().slice(0, 7))
+  const [dateClosing, setDateClosing] = useState(new Date().toISOString().slice(0, 7))
+  
 
   return (
     <section
@@ -55,8 +63,9 @@ export default function BtcLossSection() {
           </SelectTrigger>
           <SelectContent>
             <SelectGroup>
-              <SelectItem value="monthly">{t("dcaCalculator.monthly")}</SelectItem>
-              <SelectItem value="weekly">{t("dcaCalculator.weekly")}</SelectItem>
+              <SelectItem value="4">{t("dcaCalculator.monthly")}</SelectItem>
+              <SelectItem value="2">2 {t("dcaCalculator.weekly")}</SelectItem>
+              <SelectItem value="1">{t("dcaCalculator.weekly")}</SelectItem>
             </SelectGroup>
           </SelectContent>
         </Select>
@@ -66,9 +75,9 @@ export default function BtcLossSection() {
         </label>
         <Input
           name="amount"
-          type="date"
-          min="2017-01-01"
-          max={new Date().toISOString().split("T")[0]}
+          type="month"
+          min={"2017-03"}
+          max={dateClosing}
           value={dateOpening}
           onChange={(e) => setDateOpening(e.target.value)}
           className="mb-5 mt-1 block !rounded-circle border-none !text-base"
@@ -79,9 +88,9 @@ export default function BtcLossSection() {
         </label>
         <Input
           name="amount"
-          type="date"
-          min="2017-01-01"
-          max={new Date().toISOString().split("T")[0]}
+          type="month"
+          min={dateOpening}
+          max={new Date().toISOString().slice(0, 7)}
           value={dateClosing}
           onChange={(e) => setDateClosing(e.target.value)}
           className="mb-5 mt-1 block !rounded-circle  border-none !text-base"
@@ -91,8 +100,13 @@ export default function BtcLossSection() {
       </div>
 
       {/* ----------- interactive Graph */}
-      <div className="relative flex w-full flex-col pt-5 bg-backgroundMuted rounded-xlg items-center justify-start sm:w-[75%]">
-        <BtcLossGraph amount={amount} frequency={frequency} dateOpening={dateOpening} dateClosing={dateClosing} />
+      <div className="relative flex w-full flex-col items-center justify-start rounded-xlg bg-backgroundMuted pt-5 sm:w-[75%]">
+        <BtcLossGraph
+          amount={amount}
+          frequency={parseInt(frequency)}
+          dateOpening={dateOpening}
+          dateClosing={dateClosing}
+        />
       </div>
     </section>
   )
