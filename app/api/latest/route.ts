38f7ee0ee7db260bcb531f85a1c153436f0b0bb1
Roman Flössler <42189578+Roman-Flossler@users.noreply.github.com>
2025-02-11 10:23:24 +0100

    add limit to latest api call

--- app/api/latest/route.ts
+++ app/api/latest/route.ts
@@ -1,8 +1,8 @@
 export async function GET(request: Request) {
   try {
-    // const { searchParams } = new URL(request.url)
-    // const id = searchParams.get("id")
-    const res = await fetch(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=${6}`, {
+    const { searchParams } = new URL(request.url)
+    const limit = searchParams.get("limit")
+    const res = await fetch(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=${limit}`, {
       next: { revalidate: 60 * 10 }, // in seconds
       // cache: "no-cache", // Don't cache response
       // @ts-ignore
